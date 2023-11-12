2508ddaaef9200004bc8b9a34c92eba6a62aa461
lenya489 <lenya4892@gmail.com>
2023-11-12 19:12:10 +0300

    Added access by hash

--- stores/Diets.go
+++ stores/Diets.go
@@ -54,3 +54,56 @@ func GetDietWithId(id int) (schemas.Diet, error) {
 
 	return diet, nil
 }
+
+func GetDietWithHash(hash int) (schemas.Diet, error) {
+	db, err := CreateConnection()
+	if err != nil {
+		return schemas.Diet{}, err
+	}
+
+	var diet schemas.Diet
+	err = db.
+		QueryRow(`
+			select
+				diet_hash,
+				diet_name
+			from
+			    diet
+			where
+			    diet_hash = $1`,
+			hash).
+		Scan(&diet.Hash,
+			&diet.Name)
+	if err != nil {
+		return schemas.Diet{}, err
+	}
+
+	dayDietsRows, err := db.
+		Query(`
+			select
+			    diet_day_id,
+			    index
+			from
+			    diet_day_diet
+			where
+			    diet_hash = $1
+			order by
+			    index`,
+			id)
+	if err != nil {
+		return schemas.Diet{}, err
+	}
+
+	defer dayDietsRows.Close()
+
+	for dayDietsRows.Next() {
+		var dayDietId int
+		var index int
+		if err = dayDietsRows.Scan(&dayDietId, &index); err != nil {
+			return schemas.Diet{}, err
+		}
+		diet.DayDiets = append(diet.DayDiets, schemas.DayDiet{Id: dayDietId, Index: index})
+	}
+
+	return diet, nil
+}
