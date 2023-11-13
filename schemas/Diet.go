eaa411a19c2416c19a4123410ec7f9c682562f2c
lenya489 <lenya4892@gmail.com>
2023-11-13 12:03:47 +0300

    Added field hash to class Diet

--- schemas/Diet.go
+++ schemas/Diet.go
@@ -2,6 +2,7 @@ package schemas
 
 type Diet struct {
 	Id       int       `json:"id"`
+	Hash     int       `json:"hash"`
 	Name     string    `json:"name"`
 	DayDiets []DayDiet `json:"day-diets"`
 }
