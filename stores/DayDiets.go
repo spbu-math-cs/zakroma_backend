985f0ee8238d5a3ec5f945322f93e75180ef3fc8
DIvanCode <divancode@gmail.com>
2023-11-27 14:05:59 +0300

    implemented create dayDiet

--- stores/DayDiets.go
+++ stores/DayDiets.go
@@ -41,3 +41,53 @@ func GetDayDietWithId(id int) (schemas.DayDiet, error) {
 
 	return dayDiet, nil
 }
+
+func CreateDayDiet(dietId int, index int, name string) (int, error) {
+	db, err := CreateConnection()
+	if err != nil {
+		return -1, err
+	}
+
+	dayDietId := -1
+
+	rows, err := db.Query(`
+		select
+		    diet_day_id
+		from
+		    diet_day_diet
+		where
+		    diet_id = $1 and index = $2
+		limit 1
+		`, dietId, index)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		if err = rows.Scan(&dayDietId); err != nil {
+			return -1, err
+		}
+	}
+
+	if dayDietId == -1 {
+		if err = db.QueryRow(`
+			insert into
+				diet_day(diet_day_name)
+			values 
+				($1)
+			returning diet_day_id
+			`, name).Scan(&dayDietId); err != nil {
+			return -1, err
+		}
+		if err = db.QueryRow(`
+			insert into
+				diet_day_diet(diet_id, diet_day_id, index)
+			values
+				($1, $2, $3)
+			returning diet_day_id
+			`, dietId, dayDietId, index).Scan(&dayDietId); err != nil {
+			return -1, err
+		}
+	}
+
+	return dayDietId, nil
+}
