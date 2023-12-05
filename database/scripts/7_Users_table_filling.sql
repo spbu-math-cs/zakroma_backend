076e8955cea4439f11933c86f0da9ce83d3bd18c
KoltsovArtem <artemklcv@gmail.com>
2023-12-05 03:04:24 +0300

    Добавил хэши

--- database/scripts/7_Users_table_filling.sql
+++ database/scripts/7_Users_table_filling.sql
@@ -1,3 +1,3 @@
 insert into users
-values (0, 'admin', '$2a$10$YDFE2R4tVHf1gG7qSF29kuYLxZ1sk4OeWZQb7XFa0zWEDVab868QK', '1999-12-31'),
-       (1, 'Gigachad', 'ef92b7784aee0b8a8a7e4c144be76b7c72f9e436a6c5761a844e7d5e454d29c3', '2007-02-01');
+values (0, 'admin', '$2a$10$YDFE2R4tVHf1gG7qSF29kuYLxZ1sk4OeWZQb7XFa0zWEDVab868QK', '1999-12-31', '55f0d301f35c0157e25c665d9169abf99186d84756061d5a87a4e8bdcd0e7138'),
+       (1, 'Gigachad', 'ef92b7784aee0b8a8a7e4c144be76b7c72f9e436a6c5761a844e7d5e454d29c3', '2007-02-01', '43cb59219f2c150a5c9cf84eafadf1fc2262cbcd1ec13ddf43ce653da9339784');
