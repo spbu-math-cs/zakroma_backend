c4892dfb17fb1499cc0fe3cca808cd49ea5f502b
DIvanCode <divancode@gmail.com>
2023-12-10 18:36:20 +0300

    fixed bugs

--- database/scripts/4_Meals_table_filling.sql
+++ database/scripts/4_Meals_table_filling.sql
@@ -1,17 +1,17 @@
 ALTER SEQUENCE meals_meal_id_seq RESTART WITH 1;
 
 insert into meals
-values (0, 'Завтрак качка', 'd154c7edfc68899a46e017394f5427f3d4a8a5c659570e1e47be9dbdf7e4d1c6');
+values (0, 'Завтрак качка', 'd154c7edfc68899a46e017394f5427f3d4a8a5c659570e1e47be9dbdf7e4d1c6', null);
 
 insert into meals (meal_name, meal_hash, tag_id)
-values ('Перекус качка', '22157cc64dafa7000bcfe91d280818e3821b6b376aa6a9c65e9de85eadb4c047'),
-       ('Обед качка', 'ce5c5d66381d5c636f1b6be0bc417ec4e5c2c997cba4cc60810ba36aa6ad9623'),
-       ('Ужин качка', '5427a2e2b9105a6f727857f7c7cb5838a18bfb887abda8ee4b09f8c06d2ca793'),
-       ('Завтрак перед пробежкой', '5a2dd78474ab98c9f122df158edd08fe23052aa658be654cb7ae715e24d1814d'),
-       ('Перекус перед качалкой', '6f950e170171c8ee587a4891f6ec8af8e1e1723365203e6f4d12b8735514d176'),
-       ('Обед для хорошего сна', 'd8f3ae36061b4dade40e75c94573b2ca4d1315e491b2ea50eac5ce0c342f0e98'),
-       ('Ужин перед взрывной тренировкой', '00431e39782111b01e6d5a4b7dd5a020c97de7e845eea760377613c1c27b89f8'),
-       ('Завтрак "Cheat meal"', '8e866eb4a7ec7da22076f859aa87c584bc9f0fdec932ea3dc90a522bcc6a420b'),
-       ('Перекус "Cheat meal"', 'e489c83030c43c2a954122a097a55f2fe200f4ec633bef0a1ae0ea72f95d04ac'),
-       ( 'Обед "Cheat meal"', 'a7174c9ded91c375742daf7eaadfff524883158662018571f36fb136ca1af2da'),
-       ( 'Ужин "Cheat meal"', '57408602f767b565e6f2b94e9610caeaa58fa15a53e74d62dc958f8e72ced6c6');
+values ('Перекус качка', '22157cc64dafa7000bcfe91d280818e3821b6b376aa6a9c65e9de85eadb4c047', null),
+       ('Обед качка', 'ce5c5d66381d5c636f1b6be0bc417ec4e5c2c997cba4cc60810ba36aa6ad9623', null),
+       ('Ужин качка', '5427a2e2b9105a6f727857f7c7cb5838a18bfb887abda8ee4b09f8c06d2ca793', null),
+       ('Завтрак перед пробежкой', '5a2dd78474ab98c9f122df158edd08fe23052aa658be654cb7ae715e24d1814d', null),
+       ('Перекус перед качалкой', '6f950e170171c8ee587a4891f6ec8af8e1e1723365203e6f4d12b8735514d176', null),
+       ('Обед для хорошего сна', 'd8f3ae36061b4dade40e75c94573b2ca4d1315e491b2ea50eac5ce0c342f0e98', null),
+       ('Ужин перед взрывной тренировкой', '00431e39782111b01e6d5a4b7dd5a020c97de7e845eea760377613c1c27b89f8', null),
+       ('Завтрак "Cheat meal"', '8e866eb4a7ec7da22076f859aa87c584bc9f0fdec932ea3dc90a522bcc6a420b', null),
+       ('Перекус "Cheat meal"', 'e489c83030c43c2a954122a097a55f2fe200f4ec633bef0a1ae0ea72f95d04ac', null),
+       ( 'Обед "Cheat meal"', 'a7174c9ded91c375742daf7eaadfff524883158662018571f36fb136ca1af2da', null),
+       ( 'Ужин "Cheat meal"', '57408602f767b565e6f2b94e9610caeaa58fa15a53e74d62dc958f8e72ced6c6', null);
--- database/scripts/7_Users_table_filling.sql
+++ database/scripts/7_Users_table_filling.sql
@@ -1,8 +1,8 @@
 ALTER SEQUENCE users_user_id_seq RESTART WITH 1;
 
 insert into users values
-        (0, 'admin', '$2a$10$YDFE2R4tVHf1gG7qSF29kuYLxZ1sk4OeWZQb7XFa0zWEDVab868QK', '1999-12-31', '55f0d301f35c0157e25c665d9169abf99186d84756061d5a87a4e8bdcd0e7138');
+        (0, 'admin', 'adminov', 'YAVSEHNENAVIZHY@yandex.com', '$2a$10$YDFE2R4tVHf1gG7qSF29kuYLxZ1sk4OeWZQb7XFa0zWEDVab868QK', '1999-12-31', '55f0d301f35c0157e25c665d9169abf99186d84756061d5a87a4e8bdcd0e7138');
 
-insert into users (user_name, password_hash, birth_date, user_hash)
+insert into users (user_name, user_surname, user_email, password_hash, birth_date, user_hash)
 values
-       ('Gigachad', 'ef92b7784aee0b8a8a7e4c144be76b7c72f9e436a6c5761a844e7d5e454d29c3', '2007-02-01', '43cb59219f2c150a5c9cf84eafadf1fc2262cbcd1ec13ddf43ce653da9339784');
+       ('Gigachad', 'AAAAAAA', 'eeeeeeeeeee@gg.gg', 'ef92b7784aee0b8a8a7e4c144be76b7c72f9e436a6c5761a844e7d5e454d29c3', '2007-02-01', '43cb59219f2c150a5c9cf84eafadf1fc2262cbcd1ec13ddf43ce653da9339784');
