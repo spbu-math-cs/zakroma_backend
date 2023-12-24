20f29738e6020931d98c49923cd6fd3c3f5f5520
lenya489 <lenya4892@gmail.com>
2023-12-24 23:28:40 +0300

    Added tags + dishes tagged

--- database/scripts/06_Tags_for_dishes_table_filling.sql
+++ database/scripts/06_Tags_for_dishes_table_filling.sql
@@ -1,9 +1,13 @@
 ALTER SEQUENCE tags_for_dishes_tag_id_seq RESTART WITH 1;
 
 insert into tags_for_dishes
-values (0, 'breakfast');
+values (0, 'Завтрак');
 
 insert into tags_for_dishes(tag)
-values ('snack'),
-       ('lunch'),
-       ('dinner');
+values ('Перекус'),
+       ('Обед'),
+       ('Ужин'),
+       ('Мясное')
+       ('Веганское')
+       ('Вегетерианское')
+       ('Лёгкое');
