bf266cb68ce1f7d0b797ff423c927cafc00d76ac
Ivan Petrukhin <monkpatch@gmail.com>
2024-05-26 21:49:19 +0300

    Add quotes

--- autopull.sh
+++ autopull.sh
@@ -5,10 +5,10 @@ REMOTE_BRANCH=origin/$BRANCH
 DELAY=10
 
 while true; do
-	git fetch -q origin $BRANCH
-	own_commit=$(git rev-parse $BRANCH)
-	remote_commit=$(git rev-parse $REMOTE_BRANCH)
-	commits_count=$(git rev-list --count $BRANCH..$REMOTE_BRANCH)
+	git fetch -q origin "$BRANCH"
+	own_commit=$(git rev-parse "$BRANCH")
+	remote_commit=$(git rev-parse "$REMOTE_BRANCH")
+	commits_count=$(git rev-list --count "$BRANCH".."$REMOTE_BRANCH")
 
 	if [ "$own_commit" != "$remote_commit" ]; then
 		git reset -q --hard "$REMOTE_BRANCH"
