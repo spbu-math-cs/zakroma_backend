495f4f2b7e3522601955b4348f0e48f29552f006
Ivan Petrukhin <monkpatch@gmail.com>
2024-07-27 15:29:46 +0300

    Make dynamic hostname that actually depends on environment variable value

--- stores/Handler.go
+++ stores/Handler.go
@@ -2,20 +2,26 @@ package stores
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
+
 	_ "github.com/lib/pq" // postgres golang driver
 )
 
 func CreateConnection() (*sql.DB, error) {
+	db_host, env_err := os.LookupEnv("DB_HOST")
+	if env_err {
+		return nil, fmt.Errorf("could not find variable DB_HOST in env")
+	}
+	dataSourceName := fmt.Sprintf("host=%s port=5432 dbname=postgres user=postgres password=postgres sslmode=disable connect_timeout=10", db_host)
 	// Open the connection
-	db, err := sql.Open("postgres", "host=zakroma_db port=5432 dbname=postgres user=postgres password=postgres sslmode=disable connect_timeout=10")
-
+	db, err := sql.Open("postgres", dataSourceName)
 	if err != nil {
 		return nil, err
 	}
 
 	// check the connection
 	err = db.Ping()
-
 	if err != nil {
 		return nil, err
 	}
